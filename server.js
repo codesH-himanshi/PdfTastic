b14817a529bf89a00ea8fd54de3b68a43c202dea
Himanshi <himanshikashyap442@gmail.com>
2025-03-14 04:08:06 +0530

    Updated CORS settings

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 const port = 5000;
 
 app.use(cors());
+app.use(cors({ origin: "https://codesh-himanshi.github.io/PdfTastic/" }));
 app.use(express.json());
 app.use(express.static("uploads")); // Serve uploaded PDFs
 
